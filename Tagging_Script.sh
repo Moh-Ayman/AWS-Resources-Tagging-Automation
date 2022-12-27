3d2dddb1d6c3897e898b2af3ea4140077b432553
Mohamed Ayman <moh.ayman.ibrahim@gmail.com>
2022-12-27 23:57:21 +0200

    Commit Changes to Tag Structure

--- Tagging_Script.sh
+++ Tagging_Script.sh
@@ -6,69 +6,69 @@ lookupdata=$(sed 1d $lookupfile)
 for line in $lookupdata
 do
         servicename=`echo $line | cut -d "," -f1`
-        ID=`echo $line | cut -d "," -f2`
-        Tags=`echo $line | cut -d "," -f4| sed -E 's/\|/ /g; s/;/,/g'`
+        #Tags=`echo $line | cut -d "," -f4| sed -E 's/\|/ /g; s/;/,/g'`
+        Tags=`echo $line | cut -d "," -f4 | sed -E 's/\|/ /g; s/;/,/g' | sed -E 's/^/{/g; s/$/}/g' | sed -E 's/ /},{/g'`   
         
         case $servicename in
 
              ec2)
                  echo ec2a
-                 aws ec2 create-tags --resources $ID --tags $Tags
+                 aws ec2 create-tags --resources $ID --tags [$Tags]
                  ;;
 
              image)
                   echo image
-                  aws ec2 create-tags --resources $ID --tags $Tags
+                  aws ec2 create-tags --resources $ID --tags [$Tags]
                   ;;
 
              sg)
                   echo sg
-                  aws ec2 create-tags --resources $ID --tags $Tags
+                  aws ec2 create-tags --resources $ID --tags [$Tags]
                   ;;
 
              vpc)
                   echo vpc
-                  aws ec2 create-tags --resources $ID --tags $Tags
+                  aws ec2 create-tags --resources $ID --tags [$Tags]
                   ;;
 
              subnet)
                   echo subnet
-                  aws ec2 create-tags --resources $ID --tags $Tags
+                  aws ec2 create-tags --resources $ID --tags [$Tags]
                   ;;
 
              rt)
                   echo routetable
-                  aws ec2 create-tags --resources $ID --tags $Tags
+                  aws ec2 create-tags --resources $ID --tags [$Tags]
                   ;;
 
              alb)
                   echo alb
-                  aws elbv2 add-tags --resource-arns $ID --tags $Tags
+                  aws elbv2 add-tags --resource-arns $ID --tags [$Tags]
                   ;;
 
              nlb)
                   echo nlb
-                  aws elbv2 add-tags --resource-arns $ID --tags $Tags
+                  aws elbv2 add-tags --resource-arns $ID --tags [$Tags]
                   ;;
 
              tg)
                  echo tg
-                 aws elbv2 add-tags --resource-arns $ID --tags $Tags
+                 aws elbv2 add-tags --resource-arns $ID --tags [$Tags]
                  ;;
 
          listener)
                  echo listener
-                  aws elbv2 add-tags --resource-arns $ID --tags $Tags
+                  aws elbv2 add-tags --resource-arns $ID --tags [$Tags]
                   ;;
 
              ngw)
                  echo ngw
-                 aws ec2 create-tags --resources $ID --tags $Tags
+                 aws ec2 create-tags --resources $ID --tags [$Tags]
                  ;;
 
              tgw)
                  echo tgw
-                 aws ec2 create-tags --resources $ID --tags $Tags
+                 aws ec2 create-tags --resources $ID --tags [$Tags]
                  ;;
 
               s3)
@@ -79,12 +79,12 @@ do
 
             trail)
                  echo trail
-                 aws cloudtrail add-tags --resource-id $ID --tags $Tags
+                 aws cloudtrail add-tags --resource-id $ID --tags [$Tags]
                  ;;
 
               rds)
                  echo rds
-                 aws rds add-tags-to-resource --resource-name $ID --tags $Tags
+                 aws rds add-tags-to-resource --resource-name $ID --tags [$Tags]
                  ;;
 
                *)
